536659cf9cc1082036823db0e4dd175ccf3d6a88
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-10-19 20:36:05 +0700

    feat: setting ssl

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,15 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'db_clone_jira_dosc',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: 'nsfpntjfexdzvv',
+    password:
+      '527dfb0f200034d8aff663a1dd97d5cf3493c0a812177c6df11932b0212c7e47',
+    database: 'd530n8sbik3t3s',
+    host: 'ec2-52-201-72-91.compute-1.amazonaws.com',
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
   },
   test: {
     username: 'root',
@@ -21,6 +26,8 @@ module.exports = {
     host: 'ec2-52-201-72-91.compute-1.amazonaws.com',
     port: 5432,
     dialect: 'postgres',
-    ssl: true,
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
