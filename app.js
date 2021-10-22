d9faa0c07324dd3eebd3178b851f3e284b1de8c6
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-10-22 14:29:41 +0700

    add cors

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to clone Jira API ',
     version: '1.0.0',
   });
 });
-app.use(cors());
 app.use('/api', appRouter);
 
 // catch 404 and forward to error handler
